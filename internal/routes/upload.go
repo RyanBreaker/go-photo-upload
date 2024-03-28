aab1e8726be883a4bc0edd6553bc734176163d97
Ryan Breaker <ryan@breaker.rocks>
2024-03-28 16:32:30 -0500

    switch back to gin handling of form processing

--- internal/routes/upload.go
+++ internal/routes/upload.go
@@ -1,47 +1,38 @@
 package routes
 
 import (
-	"bytes"
-	"errors"
 	dbx "github.com/RyanBreaker/go-photo-upload/internal/dropbox"
 	"github.com/gin-gonic/gin"
-	"io"
 	"log/slog"
 	"net/http"
 	"path"
+	"strings"
 )
 
 func UploadRoute(router *gin.Engine) {
 	router.POST("/upload", func(c *gin.Context) {
 		slog.Info("Processing request...")
 
-		form, _ := c.Request.MultipartReader()
+		name := path.Clean(c.PostForm("name"))
 
-		var name string
-		var photos []dbx.Photo
-		for {
-			part, err := form.NextPart()
-			if err == io.EOF {
-				break
-			} else if errors.Is(err, io.ErrUnexpectedEOF) {
-				slog.Warn("Error parsing multipart form, likely canceled transfer:", err)
+		form, err := c.MultipartForm()
+		if err != nil {
+			if strings.Contains(err.Error(), "NextPart: EOF") {
+				slog.Warn("Unexpected EOF, likely due to canceled transfer")
 				return
 			}
+			slog.Error("Error processing multipart form:", err)
+			return
+		}
 
-			if part.FormName() == "name" {
-				buf, _ := io.ReadAll(part)
-				name = string(buf)
-				continue
-			} else if part.FileName() == "" {
-				// Ignore any other fields
-				continue
-			}
+		files := form.File["photos[]"]
 
-			buf := &bytes.Buffer{}
-			_, _ = io.Copy(buf, part)
+		var photos []dbx.Photo
+		for _, file := range files {
+			data, _ := file.Open()
 			photo := dbx.Photo{
-				FilePath: path.Join("/Photos", name, part.FileName()),
-				Data:     buf,
+				FilePath: path.Join("/Photos", name, file.Filename),
+				Data:     data,
 			}
 			photos = append(photos, photo)
 		}
